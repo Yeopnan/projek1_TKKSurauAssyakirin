ecee2af5ee991db9ce15f49893ee7e84a6351a30
YeopNan <ayeopnan@gmail.com>
2025-02-05 23:30:17 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+let button = document.getElementsByTagName("button")[0];
+button.addEventListener("click", function () {
+  alert(
+    "Pengurusan harta peninggalan si mati dimulakan dengan membiayai kelengkapan dan pengurusan jenazahnya, kemudian diselesaikan hutang-hutangnya kemudian dipenuhi wasiatnya (hanya satu pertiga daripadanya) kemudian baharulah diberikan baki harta tersebut secara faraid kepada waris-waris yang berhak"
+  );
+});
